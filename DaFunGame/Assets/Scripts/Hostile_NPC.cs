aad95361e41cb98605bd3dd567f623102be34b8b
Austin <meyerforge@gmail.com>
2018-04-22 12:49:06 -0700

    NPC start

---
+++ DaFunGame/Assets/Scripts/Hostile_NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hostile_NPC : MonoBehaviour 
+{
+	public float walkSpeed;
+
+
+	private Rigidbody2D npcRB;
+	private float runTime;
+
+
+	void Start () 
+	{
+		npcRB = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	private void moveUp()
+	{
+		npcRB.AddForce(Vector2 * walkSpeed);
+	}
+}
